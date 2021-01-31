48b7f6b7a54055c9871208b3b1c3888cab0325c7
Haraldo Filho <haraldo.filho@gmail.com>
2021-01-31 17:36:56 -0300

    Finished 'Lesson 3 - 19.Quiz'

---
+++ src/Lambdas.kt
@@ -0,0 +1,20 @@
+import kotlin.random.Random
+
+fun main(args: Array<String>) {
+
+    val rollDice = { sides: Int -> if (sides > 0) Random.nextInt(1, sides+1) else 0 }
+    println(rollDice(12))
+    println(rollDice(1))
+    println(rollDice(0))
+
+    val rollDice2: (Int) -> Int = { sides -> if (sides > 0) Random.nextInt(1, sides+1) else 0 }
+
+    gamePlay(12, rollDice2)
+    gamePlay(1, rollDice2)
+    gamePlay(0, rollDice2)
+
+}
+
+fun gamePlay(sides: Int, dice: (Int) -> Int) {
+    println(dice(sides))
+}
