679f680975b46db45ad01f668d5b0e8c0ef7f97e
Haraldo Filho <haraldo.filho@gmail.com>
2021-02-03 11:46:25 -0300

    Finished 'Lesson 4 - 12.Quiz'

--- src/lesson4/Spicies/Spice.kt
+++ src/lesson4/Spicies/Spice.kt
@@ -5,6 +5,14 @@ fun main(args: Array<String>) {
     val curry = Curry("curry", "mild")
     println(curry.color)
 
+    val spice1 = SpiceContainer(Curry("yellow curry", "mild"))
+    val spice2 = SpiceContainer(Curry("red curry", "spicy"))
+    val spice3 = SpiceContainer(Pepper("pepper", "spicy"))
+
+    println(spice1.label)
+    println(spice2.label)
+    println(spice3.label)
+
 }
 
 abstract class Spice(val name: String, val spiciness: String = "mild", color: SpiceColor): SpiceColor by color {
@@ -23,6 +31,10 @@ object YellowSpiceColor: SpiceColor {
     override val color = "yellow"
 }
 
+object RedSpiceColor: SpiceColor {
+    override val color = "red"
+}
+
 class Curry(name: String, spiciness: String,
             color: SpiceColor = YellowSpiceColor) : Spice(name, spiciness, color), Grinder {
 
@@ -33,4 +45,20 @@ class Curry(name: String, spiciness: String,
     override fun grind() {
     }
 
+}
+
+class Pepper(name: String, spiciness: String,
+            color: SpiceColor = RedSpiceColor) : Spice(name, spiciness, color), Grinder {
+
+    override fun prepareSpice() {
+        grind()
+    }
+
+    override fun grind() {
+    }
+
+}
+
+data class SpiceContainer(val spice: Spice) {
+    val label = spice.name
 }
