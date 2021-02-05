8cd433965f9552bce53e655c086d8c785711a3f1
Haraldo Filho <haraldo.filho@gmail.com>
2021-02-05 13:55:46 -0300

    Finished 'Lesson 5 - 7.Quiz'

--- src/lesson5/Book.kt
+++ src/lesson5/Book.kt
@@ -1,12 +1,24 @@
 package lesson5
 
+const val MAX_BORROW = 5
+
 fun main(args: Array<String>) {
     val book = Book("Fahrenheit 451", "Ray Bradbury", "1953")
     val (title, author, year) = book.getAllBookInfo()
     println("Here is your book ${title} written by ${author} in ${year}.")
+
+    println(book.canBorrow(5))
+    println(book.canBorrow(3))
+
+    book.printUrl()
+
 }
 
 class Book(val title: String, val author: String, val year: String) {
+
+    companion object {
+        const val BASE_URL = "https://www.library.net/"
+    }
 
     fun getTitleAndAuthor(): Pair<String, String> {
         return title to author
@@ -14,6 +26,14 @@ class Book(val title: String, val author: String, val year: String) {
 
     fun getAllBookInfo(): Triple<String, String, String> {
         return Triple(title, author, year)
+    }
+
+    fun canBorrow(borrowed: Int ): Boolean {
+        return borrowed < MAX_BORROW
+    }
+
+    fun printUrl() {
+        println(BASE_URL + title.replace(" ", "_") + ".html")
     }
 
 }
